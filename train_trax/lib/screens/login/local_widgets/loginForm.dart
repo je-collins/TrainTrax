930dc71ae60045e692db186f8d2de3b4152828d3
EdwardT8745 <edward.tran8745@knights.ucf.edu>
2022-01-20 13:41:24 -0500

    login page update test

---
+++ train_trax/lib/screens/login/local_widgets/loginForm.dart
@@ -0,0 +1,59 @@
+import 'package:train_trax/widgets/shadowContainer.dart';
+import 'package:flutter/material.dart';
+
+enum LoginType {
+  email,
+  google,
+}
+
+class OurLoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ShadowContainer(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical:  20.0, horizontal: 8.0),
+            child: Text(
+              "Log In",
+              style: TextStyle(
+                color: Theme.of(context).secondaryHeaderColor,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        TextFormField(
+            decoration: InputDecoration(
+            prefixIcon: Icon(Icons.alternate_email), 
+            hintText: "Email"),
+            ),
+        SizedBox(height: 20.0,),
+        TextFormField(
+          decoration: InputDecoration(
+          prefixIcon: Icon(Icons.lock_outline), 
+          hintText: "Password"),
+        ),
+        SizedBox(height: 20.0,),
+        RaisedButton(
+          child: Padding(padding: EdgeInsets.symmetric( horizontal: 100),
+          child: Text("Login In",
+          style: TextStyle(color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+          ),
+          ),
+          ),
+          onPressed: () {},
+        ),
+        FlatButton(
+          child: Text("Don't have an accounnt? Sign up here"),
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          onPressed: () {},
+        )
+        ],
+      ),
+      
+    );
+  }
+}
