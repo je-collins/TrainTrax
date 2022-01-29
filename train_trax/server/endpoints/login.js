61ac743e8a5e4f0502c8fcb1dff86cac220da78d
Quasar_Catfish <ctsn99@gmail.com>
2022-01-29 13:52:19 -0500

    update error wording

--- train_trax/server/endpoints/login.js
+++ train_trax/server/endpoints/login.js
@@ -4,11 +4,16 @@ import md5 from 'md5';
 const endpoint = (request, response) => {
 	const { email, password } = request.body;
 	
-	// Check if email or password is not declared
-	if (email === undefined || password === undefined) {
+	// Check if one or more fields is not declared
+	const undef = [];
+	if (email === undefined) undef.push('email');
+	if (password === undefined) undef.push('password');
+
+	// If undeclared field, return error
+	if (undef.length > 0) {
 		return response.status(200).json({
 			'error': 'Invalid payload',
-			'message': 'Missing either email or password field.'
+			'message': 'Missing the following field(s): ' + undef.join(', ')
 		});
 	}
 
@@ -26,8 +31,7 @@ const endpoint = (request, response) => {
 		const token = md5(new Date().getTime().toString());
 		server.query('INSERT INTO user_tokens(user_id, token, expire_time) values($1, $2, $3) ON CONFLICT (user_id) DO UPDATE SET token = $2, expire_time = $3;', [data.user_id, token, new Date().toISOString()], (error, res) => {
 			if(error) throw error;
-
-		})
+		});
 
 		return response.status(200).json({
 			'token': token,
