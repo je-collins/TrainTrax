f37b7947213e5b509c4ed141a920d376ccce1d63
Quasar_Catfish <ctsn99@gmail.com>
2022-01-29 10:49:34 -0500

    update login endpoint to return desired output

--- train_trax/server/endpoints/login.js
+++ train_trax/server/endpoints/login.js
@@ -1,19 +1,40 @@
 import server from '../server.js';
+import md5 from 'md5';
 
 const endpoint = (request, response) => {
 	const { email, password } = request.body;
-
+	
+	// Check if email or password is not declared
 	if (email === undefined || password === undefined) {
-		response.status(200).json({
-			'info': 'Invalid payload, missing either email or password'
-		});
-	} else {
-		server.query('SELECT * FROM users where email = $1 and password = $2;', [email, password], (error, results) => {
-			if (error) throw error;
-			if (results.rows.length === 0) response.status(200).json({ 'info': 'Incorrect email or password' });
-			else response.status(200).json(results.rows[0]);
+		return response.status(200).json({
+			'error': 'Invalid payload',
+			'message': 'Missing either email or password field.'
 		});
 	}
+
+	// Retrieve information from user with given email and password
+	server.query('SELECT * FROM users WHERE email = $1 and password = $2;', [email, md5(password)], (error, res) => {
+		if (error) throw error;
+		if (res.rows.length === 0) return response.status(200).json({
+			'error': 'Incorrect credentials',
+			'message': 'No user exists with the given email or password.'
+		});
+
+		const data = res.rows[0];
+
+		// Generate random token and insert into database
+		const token = md5(new Date().getTime().toString());
+		server.query('INSERT INTO user_tokens(user_id, token, expire_time) values($1, $2, $3) ON CONFLICT (user_id) DO UPDATE SET token = $2, expire_time = $3;', [data.user_id, token, new Date().toISOString()], (error, res) => {
+			if(error) throw error;
+
+		})
+
+		return response.status(200).json({
+			'token': token,
+			'error': '',
+			'message': ''
+		});
+	});
 };
 
 export default endpoint;
