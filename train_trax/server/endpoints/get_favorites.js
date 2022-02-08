b611e683c1355d41d8522b42d6146886648d2147
Quasar_Catfish <ctsn99@gmail.com>
2022-02-08 14:22:51 -0500

    rewrite get starred articles, get starred domains, and get favorties

---
+++ train_trax/server/endpoints/get_favorites.js
@@ -0,0 +1,40 @@
+import User from '../objects/User.js';
+import DB from '../objects/DB.js';
+
+export default async (request, response) => {
+	// Destructure request body into relevant variables
+	const { token } = request.body;
+
+	// Create return JSON structure
+	const json = {
+		'results': [],
+		'error': '',
+		'message': ''
+	};
+
+	// Check if one or more fields is not declared
+	const undef = [];
+	if (token === undefined) undef.push('token');
+
+	// If undeclared field, return error
+	if (undef.length > 0) {
+		json.error = 'Invalid payload';
+		json.message = `Missing the following field${undef.length === 1 ? '' : 's'}: ${undef.join(', ')}.`;
+		return response.status(400).json(json);
+	}
+
+	// Retrieve user data
+	const user = await User.fromToken(token);
+
+	// If user does not exist, return invalid credentials
+	if (user === null) {
+		json.error = 'Invalid credentials';
+		json.message = 'The token provided is either invalid or expired.';
+		return response.status(401).json(json);
+	}
+
+	// Query database for favorited articles and return
+	const rows = await DB.query('SELECT * FROM articles WHERE user_id = $1 AND is_favorite;', [user.user_id]);
+	for (const row of rows) json.results.push(row.article);
+	return response.status(200).json(json);
+};
