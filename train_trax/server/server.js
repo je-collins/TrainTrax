ae7e600279e79167621ab238c71f16c908b8803f
Quasar_Catfish <ctsn99@gmail.com>
2022-02-23 01:23:32 -0500

    Restructure server.js

--- train_trax/server/server.js
+++ train_trax/server/server.js
@@ -3,19 +3,6 @@ import bodyParser from 'body-parser';
 import 'dotenv/config';
 import express from 'express';
 
-// Endpoints
-import login from './endpoints/login.js';
-import register from './endpoints/register.js';
-import forgot_password from './endpoints/forgot_password.js';
-import reset_password from './endpoints/reset_password.js';
-import get_starred from './endpoints/get_starred.js';
-import get_favorites from './endpoints/get_favorites.js';
-import add_member from './endpoints/add_members.js';
-import delete_member from './endpoints/delete_member.js';
-import get_articles from './endpoints/get_articles.js';
-import set_favorite from './endpoints/set_favorite.js';
-import add_starred from './endpoints/add_starred.js';
-
 // Create the app
 const app = express();
 const port = process.env.PORT || 3302;
@@ -32,18 +19,37 @@ app.listen(port, function() {
 	console.log(`Server listening on port ${port}`)
 });
 
-// POST endpoints
+// Login-related Endpoints
+import login from './endpoints/login.js';
+import register from './endpoints/register.js';
 app.post('/api/login', login);
 app.post('/api/register', register);
+
+// Password-related endpoints
+import forgot_password from './endpoints/forgot_password.js';
+import reset_password from './endpoints/reset_password.js';
 app.post('/api/forgot_password', forgot_password);
 app.post('/api/reset_password', reset_password);
+
+// Get Articles/Domains endpoints
+import get_articles from './endpoints/get_articles.js';
+import get_favorites from './endpoints/get_favorites.js';
+import get_starred from './endpoints/get_starred.js';
+app.post('/api/get_articles', get_articles);
+app.post('/api/get_favorites', get_favorites);
 app.post('/api/get_starred_articles', get_starred(false));
 app.post('/api/get_starred_domains', get_starred(true));
-app.post('/api/get_favorites', get_favorites);
-app.post('/api/add_member', add_member);
-app.post('/api/delete_member', delete_member);
-app.post('/api/delete_member', get_articles);
+
+// Modify Articles/Domains endpoints
+import set_favorite from './endpoints/set_favorite.js';
+import add_starred from './endpoints/add_starred.js';
 app.post('/api/add_favorite', set_favorite(true));
 app.post('/api/remove_favorite', set_favorite(false));
 app.post('/api/add_starred_article', add_starred(false));
 app.post('/api/add_starred_domain', add_starred(true));
+
+// Team-related endpoints
+import add_member from './endpoints/add_member.js';
+import delete_member from './endpoints/delete_member.js';
+app.post('/api/add_member', add_member);
+app.post('/api/delete_member', delete_member);
