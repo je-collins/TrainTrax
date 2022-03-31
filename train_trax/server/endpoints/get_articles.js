2e22f610ea5a065f98d4e92604f869487d37bf78
Quasar_Catfish <ctsn99@gmail.com>
2022-03-31 17:31:27 -0400

    fix bug where wasnt checking that something was an array

--- train_trax/server/endpoints/get_articles.js
+++ train_trax/server/endpoints/get_articles.js
@@ -6,7 +6,6 @@ export default async (request, response) => {
 
 	// Create return JSON structure
 	const json = new Json(response, 'results');
-	json.set('results', []);
 
 	// Check if one or more fields is not declared
 	const undef = [];
@@ -17,8 +16,12 @@ export default async (request, response) => {
 	const user = await User.fromToken(token);
 	if (user === undefined) return json.badToken().send();
 
-    // Retrieve and send all articles
+    // Retrieve all articles
 	const articles = await Article.getArticles();
-	for (const row of articles) json.get('results').push(row.article);
+	const results = [];
+	for (const row of articles) results.push(row.article);
+	json.set('results', results);
+
+	// Send articles
 	return json.send();
 };
