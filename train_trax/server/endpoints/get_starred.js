8c413fd3dcae886c272afab0b4f5a2ca8d3fad78
Quasar_Catfish <ctsn99@gmail.com>
2022-04-11 16:57:26 -0400

    get_starred articles and domains were flipped

--- train_trax/server/endpoints/get_starred.js
+++ train_trax/server/endpoints/get_starred.js
@@ -18,7 +18,7 @@ export default (isDomain) => async (request, response) => {
 	if (user === undefined) return json.badCredentials().send();
 
 	// Query database for starred articles/domains and return
-	const articles = await (isDomain ? Article.getStarredArticles : Article.getStarredDomains)();
+	const articles = await (isDomain ? Article.getStarredDomains : Article.getStarredArticles)();
 	for (const row of articles) json.get('results').push(row.article);
 	return json.send();
 };
