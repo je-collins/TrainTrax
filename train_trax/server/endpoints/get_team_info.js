5928db99de9380791c3811d0d5f1f2faca33d634
Quasar_Catfish <ctsn99@gmail.com>
2022-04-12 16:02:53 -0400

    fix many bugs in get_team_info

--- train_trax/server/endpoints/get_team_info.js
+++ train_trax/server/endpoints/get_team_info.js
@@ -5,7 +5,7 @@ export default async (request, response) => {
 	const { token , team_id } = request.body;
 
 	// Create return JSON structure
-	const json = new Json('member_articles', 'team_articles', 'team_completed', 'completion_rate', response);
+	const json = new Json(response, 'member_articles', 'team_articles', 'team_completed', 'completion_rate');
 	json.set('member_articles', []);
 	json.set('team_articles', []);
 	json.set('team_completed', []);
@@ -24,38 +24,33 @@ export default async (request, response) => {
     // Get user_ids of all team members
     const user_ids = await Team.getMembersFromTeam(team_id);
 
-    let i = 0;
     let team_articles = await Article.getArticlesFromUsers(user_ids);
 
     // Filter all articles by start time
     team_articles.sort(function(a, b) { return a.start_time - b.start_time;})
-    //for (const item in all_articles) print(item.name);
 
     // Get articles for each team member
     for (const id of user_ids) {
-        i = 0;
         const list = [];
-        for (const article in team_articles){
-            if(article.user_id === id){
-                console.log(id);
+        for (const article of team_articles) {
+            if (article.user_id === id) {
                 list.push({
-                    'id': article.article_id,
+                    'article_id': article.article_id,
                     'article': article.article
                 });
-                i++;
             }
             if (list.length === 5) break;
         }
         json.get('member_articles').push({
-            'user': id,
+            'user_id': id,
             'articles': list
         });        
     }
 
     // Take top 5 most recently started articles
-    for (const item in team_articles.slice(0, 5)) {
+    for (const item of team_articles.slice(0, 5)) {
         json.get('team_articles').push({
-            'id': item.article_id,
+            'article_id': item.article_id,
             'article': item.article
         });
     }
@@ -64,16 +59,16 @@ export default async (request, response) => {
     // What if they don't have a complete time?
     team_articles.sort(function(a, b) { return a.complete_time - b.complete_time;})
     for (const item of team_articles.slice(0, 5)) {
-        if(item.complete_time !== null) {
-            json.team_completed.push({
-                'id': item.article_id,
+        if (item.complete_time !== null) {
+            json.get('team_completed').push({
+                'article_id': item.article_id,
                 'article': item.article
             });
         }
     }
 
     // Count of completed / count of all
-    const complete = team_articles.filter (({complete_time}) => complete_time !== null).length
+    const complete = team_articles.filter(({complete_time}) => complete_time !== null).length
     json.set('completion_rate') = complete / team_articles.length;
 	return json.send();
 };
