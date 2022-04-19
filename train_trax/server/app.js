e1dd2475ca6f180e7dd4f91180bdec5f60774aec
Quasar_Catfish <ctsn99@gmail.com>
2022-04-19 19:25:45 -0400

    fix issue with __dirname not existing

--- train_trax/server/app.js
+++ train_trax/server/app.js
@@ -1,9 +1,12 @@
 // Requires
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config({
     path: path.resolve(__dirname, './.env')
 });
